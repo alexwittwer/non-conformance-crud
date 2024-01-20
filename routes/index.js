3dd34c42411f452e0e2c249ba3c91a43a89697fa
alexwittwer <wittweral@gmail.com>
2024-01-20 16:42:20 -0500

    update styling shell

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.redirect("/collections");
+  res.render("index");
 });
 
 module.exports = router;
