948a38bea017332e9c23b7ba6482081497c66098
alexwittwer <wittweral@gmail.com>
2024-01-21 20:54:11 -0500

    fix pernacious typo

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -2,30 +2,37 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.product_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
 
 exports.product_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_detail = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
+
 exports.product_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product create GET");
 });
--- controller/productinstance_controller.js
+++ controller/productinstance_controller.js
@@ -2,33 +2,33 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.productinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_detail = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
 
 exports.productinstance_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.send("NOT IMPLEMENTED: Product Instance create GET");
 });
